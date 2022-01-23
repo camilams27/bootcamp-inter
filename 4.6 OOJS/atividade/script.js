db337bc452a01b1abeb7cb0ad5becb88284c3b30
Camila Silva <silva.camilam27@gmail.com>
2022-01-23 13:56:32 -0300

    aula js POOJS

---
+++ 4.6 OOJS/atividade/script.js
@@ -0,0 +1,74 @@
+class ContaBancaria{
+    constructor(agencia, numero, tipo, saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    set saldo(valor){
+        return this._saldo = valor;
+    }
+
+    sacar(valor){
+        if(valor > this._saldo){
+            return "operação inválida";
+        }
+        this._saldo = this._saldo - valor;
+        
+        return this._saldo;
+    }
+
+    depositar(valor){
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartaoCredito){
+        super(agencia, numero);
+        
+        this.tipo = "Conta Corrente";
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor){
+        return this._cartaoCredito = valor;
+    }
+}
+
+class ContaPoupança extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        
+        this.tipo = "Conta Poupança";
+    }
+ 
+}
+
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        
+        this.tipo = "Conta Universitária";
+    }
+    
+    sacar(valor){
+        if(valor>500){
+            return "operação inválida."
+        }
+
+        this._saldo = this._saldo - valor
+        return this._saldo;
+    }   
+}
